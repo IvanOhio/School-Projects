b74b4bf5ebca0f815f5768b7391292bd6dc33e1b
IvanOhio <id339223@ohio.edu>
2024-08-30 17:52:15 -0400

    Add files via upload
    First C++ program

---
+++ lab1.cc
@@ -0,0 +1,15 @@
+
+//program: firstProgram.cc
+//Name: Ivan Dotson
+//Date: 8/30/2014
+//Email: id339223@ohio.edu
+//Description: This program prints a message to the screen.
+
+
+#include <iostream>
+using namespace std;
+int main ( )
+{
+    cout << "Hello Me! \n How am I today? \n";
+    return (0); 
+}
